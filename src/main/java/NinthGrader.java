d5801f26868b98aac14d7822491085278134bd8c
gunner10z-school <24jzweiback@athenian.org>
2021-10-27 14:03:13 -0700

    asdf

--- src/main/java/NinthGrader.java
+++ src/main/java/NinthGrader.java
@@ -1,3 +1,52 @@
-public class NinthGrader { // SOME CODE NEEDED IN THIS LINE
-    // YOUR CODE HERE
+public class NinthGrader extends Person {
+    // Instance variables:
+    // firstName and lastName are already inherited
+    private int classYear;
+
+    // Constructors
+    // A constructor that takes three parameters for the
+    // firstName, lastName, and classYear
+    public NinthGrader(String a, String b, int c) {
+        super(a, b);
+        this.classYear = c;
+    }
+
+    // A constructor that assumes that a default student is a ninth-grader
+    public NinthGrader(String a, String b) {
+        super(a, b);
+        this.classYear = 2025;
+    }
+
+    // A default/no-argument constructor
+    public NinthGrader() {
+        super("Jerry", "Seinfeld");
+        this.classYear = 2024;
+    }
+
+
+    // doSomething() method
+    public void doSomething() {
+        System.out.println("I'm studying!");
+    }
+
+    // toString()
+    public String toString() {
+        String str = this.getFirstName() + " " + this.getLastName() + ", " + this.classYear;
+        return str;
+    }
+
+
+
+    // Getters
+    // We've already inherited getFirstName() and getLastName()
+    // so we only need to add getClassYear
+    public int getClassYear() {
+        return this.classYear;
+    }
+
+    // Setters
+    public void setClassYear(int classYear) {
+        this.classYear = classYear;
+    }
 }
+
--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,31 @@
 public class Person {
+    private String firstName;
+    private String lastName;
+    public Person(String firstName, String lastName){
+        this.firstName=firstName;
+        this.lastName=lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void doSomething(){
+        System.out.println("You speak to yo mother with that mouth");
+    }
+    @Override
+    public String toString() {
+        return this.firstName + " " + this.lastName;
+    }
 }
